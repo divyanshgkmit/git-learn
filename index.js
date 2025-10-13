69e59ce7f100d878df30cd5cc5cca8f820a8cd3c
Divyansh Jain <divyanshjain@gkmit.co>
2025-10-13 13:27:49 +0530

    new updates

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
 console.log("This is index file and edited with branch dev-divyansh");
-console.log("This is index file and edited by branch dev-git and make some changes");
-console.log("Hello World!");
-console.log("Console Log Message");
-console.log("Hello World will remove later");
